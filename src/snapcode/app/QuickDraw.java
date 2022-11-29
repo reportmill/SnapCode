d9dd7200d02a38b3b3cbc4e4cca64c817b0afb0a
Jeff Martin <jeff@reportmill.com>
2022-11-29 17:21:55 -0600

    QuickDraw: Added ShowGrid, GridSpacing and mouse coords

--- src/snapcode/app/QuickDraw.java
+++ src/snapcode/app/QuickDraw.java
@@ -2,10 +2,15 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package snapcode.app;
+import snap.geom.Point;
+import snap.geom.Rect;
 import snap.gfx.Color;
+import snap.gfx.Font;
 import snap.gfx.Painter;
+import snap.gfx.Stroke;
 import snap.util.ArrayUtils;
 import snap.view.View;
+import snap.view.ViewEvent;
 import java.util.Arrays;
 
 /**
@@ -13,11 +18,32 @@ import java.util.Arrays;
  */
 public class QuickDraw extends View {
 
+    // Whether to draw the grid
+    private boolean _showGrid;
+
+    // Whether to draw the grid
+    private double  _gridSpacing = DEFAULT_GRID_SPACING;
+
     // The pens
     private QuickDrawPen[]  _pens = new QuickDrawPen[1];
 
     // Whether this view is animating
     private QuickDrawPenAnim[]  _animPens = new QuickDrawPenAnim[0];
+
+    // A run to trigger anim pen updates
+    private Runnable  _repaintRun = () -> updateAnimPens();
+
+    // The last mouse coords
+    private int  _mouseX, _mouseY;
+
+    // Constants
+    private static final Color GRID_COLOR = Color.GRAY9;
+    private static final Stroke GRID_STROKE = Stroke.Stroke1;
+    private static final Color COORDS_COLOR = Color.GRAY6;
+    private static final Font COORDS_FONT = Font.Arial11;
+    private static final double DEFAULT_GRID_SPACING = 50;
+    private static final Color MOUSE_COORDS_COLOR = Color.GRAY4;
+    private static final Font MOUSE_COORDS_FONT = Font.Arial12;
 
     /**
      * Constructor.
@@ -28,6 +54,35 @@ public class QuickDraw extends View {
         setBorder(Color.GRAY9, 2);
         setBorderRadius(4);
         setFill(Color.WHITE);
+        setShowGrid(true);
+        enableEvents(MouseMove, MouseExit);
+    }
+
+    /**
+     * Returns whether to draw coordinate grid.
+     */
+    public boolean isShowGrid()  { return _showGrid; }
+
+    /**
+     * Sets whether to draw coordinate grid.
+     */
+    public void setShowGrid(boolean aValue)
+    {
+        _showGrid = aValue;
+        repaint();
+    }
+
+    /**
+     * Returns the space between gridlines in points.
+     */
+    public double getGridSpacing()  { return _gridSpacing; }
+
+    /**
+     * Sets the space between gridlines in points.
+     */
+    public void setGridSpacing(double aValue)
+    {
+        _gridSpacing = aValue;
     }
 
     /**
@@ -117,13 +172,88 @@ public class QuickDraw extends View {
     @Override
     protected void paintFront(Painter aPntr)
     {
+        // Paint grid
+        if (isShowGrid())
+            paintGrid(aPntr);
+
+        // Clip
+        aPntr.clipRect(0, 0, getWidth(), getHeight());
+
+        // Paint Pens
         for (QuickDrawPen pen : _pens)
             if (pen != null)
                 pen.paintPaths(aPntr);
     }
 
-    // A run to trigger anim pen updates
-    private Runnable _repaintRun = () -> updateAnimPens();
+    /**
+     * Draws the coordinate system.
+     */
+    private void paintGrid(Painter aPntr)
+    {
+        // Set grid color/stroke
+        aPntr.setPaint(GRID_COLOR);
+        aPntr.setStroke(GRID_STROKE);
+
+        // Draw grid lines
+        double viewW = getWidth();
+        double viewH = getHeight();
+        double gridSpacing = getGridSpacing();
+        for (double lineX = gridSpacing; lineX <= viewW; lineX += gridSpacing)
+            aPntr.drawLine(lineX, 0, lineX, viewH);
+        for (double lineY = gridSpacing; lineY <= viewH; lineY += gridSpacing)
+            aPntr.drawLine(0, lineY, viewW, lineY);
+
+        // Set text color/font
+        aPntr.setPaint(COORDS_COLOR);
+        aPntr.setFont(COORDS_FONT);
+        double coordsStrH = Math.ceil(COORDS_FONT.getAscent());
+
+        // Draw (width,0) and (0,height) coord labels
+        String widthCoordStr = "( " + (int) viewW + ", 0 )";
+        double widthCoordStrW = COORDS_FONT.getStringAdvance(widthCoordStr);
+        aPntr.drawString(widthCoordStr, viewW - widthCoordStrW - 4, coordsStrH + 4);
+        String heightCoordStr = "( 0, " + (int) viewH + " )";
+        aPntr.drawString(heightCoordStr, 4, viewH - 6);
+
+        // Set mouse coords color
+        aPntr.setPaint(MOUSE_COORDS_COLOR);
+        aPntr.setFont(MOUSE_COORDS_FONT);
+
+        // Draw mouse coords label
+        if (_mouseX > 0 && _mouseY > 0 && _mouseX < viewW && _mouseY < viewH) {
+            String mouseCoordsStr = "( " + _mouseX + ", " + _mouseY + " )";
+            double mouseCoordsStrH = Math.ceil(MOUSE_COORDS_FONT.getAscent());
+            double mouseCoordsStrX = _mouseX + 10;
+            double mouseCoordsStrY = _mouseY + mouseCoordsStrH + 10;
+            aPntr.drawString(mouseCoordsStr, mouseCoordsStrX, mouseCoordsStrY);
+        }
+    }
+
+    /**
+     * Process event.
+     */
+    protected void processEvent(ViewEvent anEvent)
+    {
+        // Handle MouseEvent
+        if(anEvent.isMouseEvent() && isShowGrid()) {
+
+            // Handle MouseMove
+            if(anEvent.isMouseMove()) {
+                Rect repaintRect = Rect.getRectForPoints(anEvent.getPoint(), new Point(_mouseX, _mouseY));
+                repaintRect.inset(-80, -25);
+                repaint(repaintRect);
+            }
+            _mouseX = (int) anEvent.getX();
+            _mouseY = (int) anEvent.getY();
+
+            // Handle MouseExit
+            if (anEvent.isMouseExit()) {
+                _mouseX = -1;
+                _mouseY = -1;
+                repaint(0, 0, getWidth() + 80, getHeight() + 30);
+            }
+        }
+    }
 
     /**
      * Updates animations.
