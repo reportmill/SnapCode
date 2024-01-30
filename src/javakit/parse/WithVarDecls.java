ece449e97244e35c446ca7d74ce6ead0f4ba645f
Jeff Martin <jeff@reportmill.com>
2024-01-30 09:06:30 -0600

    WithVarDecls: Fixed getVarDeclForId() to allow fields defined after id but still valid

--- src/javakit/parse/WithVarDecls.java
+++ src/javakit/parse/WithVarDecls.java
@@ -40,11 +40,74 @@ public interface WithVarDecls {
         if (varDecl == null)
             return null;
 
+        // If var decl is field, return make sure it's valid for id
+        if (varDecl.getParent() instanceof JFieldDecl) {
+            if (isFieldRefValidForId(varDecl, anId))
+                return varDecl;
+            return null;
+        }
+
         // If id appears before var decl end, return null
         if (anId.getStartCharIndex() < varDecl.getEndCharIndex())
             return null;
 
         // Return
         return varDecl;
+    }
+
+    /**
+     * Returns whether id is valid for given field var decl.
+     */
+    static boolean isFieldRefValidForId(JVarDecl varDecl, JExprId exprId)
+    {
+        JFieldDecl fieldDecl = (JFieldDecl) varDecl.getParent();
+
+        // Get enclosing body decl matching main class
+        JMemberDecl bodyDecl = exprId.getParent(JMemberDecl.class);
+        while (bodyDecl != null && bodyDecl.getEnclosingClassDecl() != fieldDecl.getEnclosingClassDecl())
+            bodyDecl = bodyDecl.getParent(JMemberDecl.class);
+
+        // Just return false if no body decl (impossible?)
+        if (bodyDecl == null)
+            return false;
+
+        // If constructor, return true
+        if (bodyDecl instanceof JConstrDecl)
+            return true;
+
+        // If method, check static
+        if (bodyDecl instanceof JMethodDecl) {
+
+            // If field static, return true (static fields always accessible from any method)
+            if (fieldDecl.getMods().isStatic())
+                return true;
+
+            // Return true if method and field are instance
+            JMethodDecl methodDecl = (JMethodDecl) bodyDecl;
+            return !methodDecl.getMods().isStatic();
+        }
+
+        // If initializer, check static
+        if (bodyDecl instanceof JInitializerDecl) {
+
+            // If field static, return true (static fields always accessible from any method)
+            if (fieldDecl.getMods().isStatic())
+                return true;
+
+            // Return true if method and field are instance
+            JInitializerDecl initDecl = (JInitializerDecl) bodyDecl;
+            return !initDecl.getMods().isStatic();
+        }
+
+        // If field, check static and location
+        if (bodyDecl instanceof JFieldDecl) {
+            JFieldDecl otherField = (JFieldDecl) bodyDecl;
+            if (fieldDecl.getMods().isStatic())
+                return !otherField.getMods().isStatic() || varDecl.getStartCharIndex() < exprId.getEndCharIndex();
+            return !otherField.getMods().isStatic() && varDecl.getStartCharIndex() < exprId.getEndCharIndex();
+        }
+
+        // Return not valid
+        return false;
     }
 }
