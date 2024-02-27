3c3e21d2753c7f5ed4274bee54154b332860a786
Jeff Martin <jeff@reportmill.com>
2024-02-27 08:47:57 -0600

    JClassDecl: Fixed silly missing assignment

--- src/javakit/parse/JClassDecl.java
+++ src/javakit/parse/JClassDecl.java
@@ -386,7 +386,7 @@ public class JClassDecl extends JMemberDecl implements WithVarDeclsX, WithTypeVa
         String className = getClassName();
         JavaClass javaClass = className != null ? getJavaClassForName(className) : null;
         if (javaClass == null)
-            new JavaClass(getResolver(), this);
+            javaClass = new JavaClass(getResolver(), this);
         return _javaClass = javaClass;
     }
 
