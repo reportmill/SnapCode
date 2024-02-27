bacac9208e659abb7d1774149c15b0bd325c2fd4
Jeff Martin <jeff@reportmill.com>
2024-02-27 13:40:26 -0600

    JavaFileBuilderX: Fixed buildFile() to checkErrors on Jepl files

--- src/snapcode/project/JavaFileBuilderX.java
+++ src/snapcode/project/JavaFileBuilderX.java
@@ -118,6 +118,15 @@ public class JavaFileBuilderX extends JavaFileBuilder {
      */
     protected boolean buildFile(WebFile sourceFile)
     {
+        // If Jepl file, check for errors first
+        if (sourceFile.getType().equals("jepl")) {
+            boolean checkErrorsSuccess = super.buildFile(sourceFile);
+            if (!checkErrorsSuccess) {
+                addBuildFile(sourceFile);
+                return false;
+            }
+        }
+
         // Compile file
         boolean compileSuccess = _compiler.compileFile(sourceFile);
 
