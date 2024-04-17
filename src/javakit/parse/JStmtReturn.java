6cb95f0f76292329b46b0a392bed976a93826bce
Jeff Martin <jeff@reportmill.com>
2024-04-17 16:58:24 -0500

    JStmtReturn: Quick stab at implementing error check

--- src/javakit/parse/JStmtReturn.java
+++ src/javakit/parse/JStmtReturn.java
@@ -2,6 +2,8 @@
  * Copyright (c) 2010, ReportMill Software. All rights reserved.
  */
 package javakit.parse;
+import javakit.resolver.JavaClass;
+import javakit.resolver.JavaType;
 
 /**
  * A Java statement for return statement.
@@ -32,4 +34,35 @@ public class JStmtReturn extends JStmt {
         replaceChild(_expr, _expr = anExpr);
     }
 
+    /**
+     * Override to check errors.
+     */
+    @Override
+    protected NodeError[] getErrorsImpl()
+    {
+        // Handle Constructor
+        JExecutableDecl executableDecl = getParent(JExecutableDecl.class);
+        if (executableDecl instanceof JConstrDecl) {
+            if (_expr != null)
+                return NodeError.newErrorArray(_expr, "Unexpected return value");
+            return NodeError.NO_ERRORS;
+        }
+
+        // Handle Method
+        JType returnJType = ((JMethodDecl) executableDecl).getReturnType();
+        JavaType returnType = returnJType != null ? returnJType.getJavaType() : null;
+        JavaClass returnClass = returnType != null ? returnType.getEvalClass() : null;
+        JavaType exprType = _expr != null ? _expr.getEvalType() : getJavaClassForName("void");
+        JavaClass exprClass = exprType.getEvalClass();
+
+        // If types don't match, return error
+        if (returnClass == null || !returnClass.isAssignableFrom(exprClass)) {
+            String returnClassName = returnClass != null ? returnClass.getClassName() : "null";
+            String msg = "Incompatible types: " + exprType.getClassName() + " cannot be converted to " + returnClassName;
+            return NodeError.newErrorArray(_expr != null ? _expr : this, msg);
+        }
+
+        // Return no errors
+        return NodeError.NO_ERRORS;
+    }
 }
