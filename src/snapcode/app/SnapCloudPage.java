d997d7701d1045f3b4eb4648db3f4645ffd33eb5
Jeff Martin <jeff@reportmill.com>
2025-05-07 14:50:35 -0500

    SnapCloudPage: Added support to show all cloud files HomePageView: Added button to show SnapCloudPage

--- src/snapcode/app/SnapCloudPage.java
+++ src/snapcode/app/SnapCloudPage.java
@@ -1,13 +1,20 @@
 package snapcode.app;
 import snap.gfx.Image;
-import snap.view.Label;
+import snap.view.*;
+import snap.web.WebFile;
+import snap.web.WebSite;
 import snap.web.WebURL;
+import snapcode.apptools.AccountTool;
+import snapcode.webbrowser.WebBrowser;
 import snapcode.webbrowser.WebPage;
 
 /**
  * This page helps manage default cloud storage.
  */
 public class SnapCloudPage extends WebPage {
+
+    // The WebBrowser for remote files
+    private WebBrowser _remoteBrowser;
 
     /**
      * Constructor.
@@ -17,13 +24,115 @@ public class SnapCloudPage extends WebPage {
         super();
     }
 
+    /**
+     * Connect to remote site.
+     */
+    public void connectToRemoteSite()
+    {
+        // Get remote site
+        WebSite snapCloudUserSite = getSnapCloudUserSite();
+        if (snapCloudUserSite == null) {
+            _remoteBrowser.setSelFile(null);
+            return;
+        }
+
+        // Reset remote site root dir
+        WebFile rootDir = snapCloudUserSite.getRootDir();
+        rootDir.resetAndVerify();
+
+        // Set root dir in remote browser
+        _remoteBrowser.setSelFile(rootDir);
+    }
+
+    /**
+     * Initialize UI.
+     */
     @Override
     protected void initUI()
     {
+        getUI().setFill(ViewTheme.get().getContentColor());
+
         // Configure label
         Label snapCloudLabel = getView("SnapCloudLabel", Label.class);
         WebURL snapCloudImageUrl = WebURL.getURL("https://reportmill.com/SnapCode/images/SnapCloud.png");
         Image snapCloudImage = Image.getImageForUrl(snapCloudImageUrl);
         snapCloudLabel.setImage(snapCloudImage);
+
+        // Get WebBrowser for remote files
+        _remoteBrowser = new WebBrowser();
+        _remoteBrowser.addPropChangeListener(e -> resetLater(), WebBrowser.Loading_Prop);
+
+        // Add to RemoteBrowserBox
+        BoxView remoteBrowserBox = getView("RemoteBrowserBox", BoxView.class);
+        remoteBrowserBox.setContent(_remoteBrowser);
+    }
+
+    /**
+     * Initialize showing.
+     */
+    @Override
+    protected void initShowing()
+    {
+        runDelayed(this::connectToRemoteSite, 1000);
+    }
+
+    /**
+     * Reset UI.
+     */
+    @Override
+    protected void resetUI()
+    {
+        // If user email hasn't been set, show email box
+        String userEmail = AccountTool.getUserEmail();
+        setViewVisible("EmailBox", userEmail == null || userEmail.isEmpty());
+
+        // Update ProgressBar
+        ProgressBar progressBar = getView("ProgressBar", ProgressBar.class);
+        boolean loading = _remoteBrowser.isLoading();
+        System.out.println("Remote browser loaded: " + loading);
+        progressBar.setVisible(loading);
+        progressBar.setProgress(loading ? -1 : 0);
+    }
+
+    /**
+     * Respond UI.
+     */
+    @Override
+    protected void respondUI(ViewEvent anEvent)
+    {
+        super.respondUI(anEvent);
+    }
+
+    /**
+     * Returns the snap cloud URL for account user email.
+     */
+    public static WebSite getSnapCloudUserSite()
+    {
+        WebURL snapCloudUserUrl = getSnapCloudUserUrl();
+        if (snapCloudUserUrl == null)
+            return null;
+        return snapCloudUserUrl.getAsSite();
+    }
+
+    /**
+     * Returns the snap cloud URL for account user email.
+     */
+    public static WebURL getSnapCloudUserUrl()
+    {
+        String userEmail = AccountTool.getUserEmail();
+        if (userEmail == null || userEmail.isEmpty())
+            return null;
+
+        int sepIndex = userEmail.indexOf('@');
+        if (sepIndex <= 0)
+            return null;
+
+        // Get username and domain
+        String userName = userEmail.substring(0, sepIndex);
+        String domain = userEmail.substring(sepIndex + 1);
+
+        // Return
+        String snapCloudUrlAddress  = "dbox://dbox.com/" + domain + "/" + userName;
+        return WebURL.getURL(snapCloudUrlAddress);
     }
 }
