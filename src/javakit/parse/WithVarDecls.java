d233b036ef23190c7fdbcd79ce6ab5dddbd77b8b
Jeff Martin <jeff@reportmill.com>
2025-07-23 11:17:11 -0500

    WithVarDecls: Fixed isFieldVarDeclValidForNode() with inner classes

--- src/javakit/parse/WithVarDecls.java
+++ src/javakit/parse/WithVarDecls.java
@@ -41,7 +41,7 @@ public interface WithVarDecls {
 
         // If var decl is field, return make sure it's valid for id
         if (varDecl.getParent() instanceof JFieldDecl) {
-            if (isFieldRefValidForId(varDecl, anId))
+            if (isFieldVarDeclValidForNode(varDecl, anId))
                 return varDecl;
             return null;
         }
@@ -55,58 +55,45 @@ public interface WithVarDecls {
     }
 
     /**
-     * Returns whether id is valid for given field var decl.
+     * Returns whether given field var decl is valid for given node.
      */
-    static boolean isFieldRefValidForId(JVarDecl varDecl, JExprId exprId)
+    static boolean isFieldVarDeclValidForNode(JVarDecl varDecl, JNode otherNode)
     {
         JFieldDecl fieldDecl = (JFieldDecl) varDecl.getParent();
 
-        // Get enclosing body decl matching main class
-        JMemberDecl bodyDecl = exprId.getParent(JMemberDecl.class);
-        while (bodyDecl != null && bodyDecl.getEnclosingClassDecl() != fieldDecl.getEnclosingClassDecl())
-            bodyDecl = bodyDecl.getParent(JMemberDecl.class);
-
-        // Just return false if no body decl (impossible?)
-        if (bodyDecl == null)
+        // Get enclosing member for other node
+        JMemberDecl otherNodeMember = otherNode.getParent(JMemberDecl.class);
+        if (otherNodeMember == null) // Impossible?
             return false;
 
-        // If constructor, return true
-        if (bodyDecl instanceof JConstrDecl)
-            return true;
-
-        // If method, check static
-        if (bodyDecl instanceof JMethodDecl) {
-
-            // If field static, return true (static fields always accessible from any method)
-            if (fieldDecl.getModifiers().isStatic())
-                return true;
-
-            // Return true if method and field are instance
-            JMethodDecl methodDecl = (JMethodDecl) bodyDecl;
-            return !methodDecl.getModifiers().isStatic();
-        }
-
-        // If initializer, check static
-        if (bodyDecl instanceof JInitializerDecl) {
-
-            // If field static, return true (static fields always accessible from any method)
-            if (fieldDecl.getModifiers().isStatic())
-                return true;
-
-            // Return true if method and field are instance
-            JInitializerDecl initDecl = (JInitializerDecl) bodyDecl;
-            return !initDecl.getModifiers().isStatic();
+        // If other node member not in same class as given field, check parent member first
+        boolean isOtherNodeInNestedClass = otherNodeMember.getEnclosingClassDecl() != fieldDecl.getEnclosingClassDecl();
+        if (isOtherNodeInNestedClass && !otherNodeMember.isStatic()) {
+            if (!isFieldVarDeclValidForNode(varDecl, otherNodeMember))
+                return false;
         }
 
         // If field, check static and location
-        if (bodyDecl instanceof JFieldDecl) {
-            JFieldDecl otherField = (JFieldDecl) bodyDecl;
-            if (fieldDecl.getModifiers().isStatic())
-                return !otherField.getModifiers().isStatic() || varDecl.getStartCharIndex() < exprId.getEndCharIndex();
-            return !otherField.getModifiers().isStatic() && varDecl.getStartCharIndex() < exprId.getEndCharIndex();
+        if (otherNodeMember instanceof JFieldDecl otherNodeField) {
+
+            // If given field static, return true if other node not static or declared after
+            if (fieldDecl.isStatic()) {
+                if (!otherNodeField.isStatic())
+                    return true;
+                return varDecl.getStartCharIndex() < otherNode.getEndCharIndex();
+            }
+
+            // If given field not static, return true if other node not static and declared after
+            if (otherNodeField.isStatic())
+                return false;
+            return isOtherNodeInNestedClass || varDecl.getStartCharIndex() < otherNode.getEndCharIndex();
         }
 
-        // Return not valid
-        return false;
+        // If other (method, constructor, initializer, inner-class), return true if field is static or member is not static
+        if (fieldDecl.isStatic())
+            return true;
+
+        // Return true if other is also not static
+        return !otherNodeMember.isStatic();
     }
 }
