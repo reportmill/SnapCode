9a2587741c4a9a55a04018cc127aa191702a1b53
Jeff Martin <jeff@reportmill.com>
2026-08-10 10:49:20 -0500

    Monly build updates

--- src/snapcode/project/BuildFile.java
+++ src/snapcode/project/BuildFile.java
@@ -348,7 +348,7 @@ public class BuildFile extends PropObject {
 
         // Handle Add SnapCharts
         if (aValue)
-            addDependency(new MavenDependency("com.reportmill:snapcharts:2026.07"));
+            addDependency(new MavenDependency("com.reportmill:snapcharts:2026.08"));
 
         // Handle Remove SnapCharts
         else {
--- src/snapcode/util/HelpFile.md
+++ src/snapcode/util/HelpFile.md
@@ -492,7 +492,7 @@ org.jbox2d.testbed.framework.TestbedMain.main(new String[0]);
 ## ReportMill
 
 ```
-//DEPS com.reportmill:ReportMill16:2026.07
+//DEPS com.reportmill:ReportMill16:2026.08
 import com.reportmill.base.*;
 import com.reportmill.shape.RMDocument;
 import java.io.File;
